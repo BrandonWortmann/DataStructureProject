e36d6bd73e120a52050cf800aac748da85def37a
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2018-01-31 12:11:57 -0700

    Added a timer classs

--- DataStructureProject/Controller/Controller.cpp
+++ DataStructureProject/Controller/Controller.cpp
@@ -12,6 +12,17 @@ using namespace std;
 
 void Controller :: start()
 {
-    
-    
+    Timer codeTimer;
+    codeTimer.startTimer();
+    cout << "Lookcode on the screen" << endl;
+    codeTimer.stopTimer();
+    codeTimer.displayInformation();
+    codeTimer.resetTimer();
+    codeTimer.startTimer();
+    for(int i = 0; i<50000; i++)
+    {
+        cout << "Index is " << i << "\t";
+    }
+    codeTimer.stopTimer();
+    codeTimer.displayInformation();
 }
