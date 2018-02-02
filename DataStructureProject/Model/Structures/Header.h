f86c5794dccbbfa4f7376e4a5300183953c70826
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2018-02-02 12:21:29 -0700

    i added a new header for the array object an added some of the methods

---
+++ DataStructureProject/Model/Structures/Header.h
@@ -0,0 +1,65 @@
+//
+//  Header.h
+//  DataStructureProject
+//
+//  Created by Wortmann, Brandon on 2/2/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class Aarray
+{
+private:
+    Type * internalArray;
+    int size;
+    
+public:
+    Array<Type>(int size);
+    
+    array<Type>(const Array<Type> & toCopy);
+    
+    ~Array<Type>();
+    
+    Array<Type & operator = (const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    
+    int getSize() const;
+    Type getFromIndex(int index);
+    void setAtIndex(int index, Type data);
+    
+}
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    this -> size = size;
+    
+    internalArray = new Type[size]
+        
+}
+
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+    this->size = toCopy.getSize();
+    
+    internalArray = new Type[size];
+    
+    for(int index = 0; index < size; index++)
+    {
+        internalArray[index] = toCopy[index];
+    }
+    
+}
+    
+
+#endif /* Header_h */
