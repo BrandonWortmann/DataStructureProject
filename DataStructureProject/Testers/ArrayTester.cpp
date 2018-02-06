a4ed5cbf4fd9a449468c0a94e04d0be1d1bb5e16
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2018-02-06 12:58:37 -0700

    I wrote the array tester advanced method

--- DataStructureProject/Testers/ArrayTester.cpp
+++ DataStructureProject/Testers/ArrayTester.cpp
@@ -46,4 +46,27 @@ void ArrayTester :: testAdvancedArray()
 {
     vector<CrimeData> test = FileController :: readCrimeDataToVector("/Users/bwor0809/Documents/C++/DataStructureProject/DataStructureProject/Data/crime.csv");
     
+    int arraySize = test.size();
+    
+    Array<CrimeData> data(arraySize);
+    for(int index = 0; index < arraySize; index++)
+    {
+        data[index] = test[index];
+    }
+    
+    Timer vectorTimer;
+    Timer arrayTimer;
+    
+    vectorTimer.startTimer();
+    cout << test[3425] << endl;
+    vectorTimer.stopTimer();
+    vectorTimer.displayInformation();
+    
+    arrayTimer.startTimer();
+    cout << data[3425] << endl;
+    arrayTimer.stopTimer();
+    arrayTimer.displayInformation();
+    
+    
+    
 }
