b1621555374682e78118b5b0579333e218dc7ad3
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2018-02-08 13:21:27 -0700

    i wrote the list class

---
+++ DataStructureProject/Model/Structures/List.hpp
@@ -0,0 +1,33 @@
+//
+//  List.hpp
+//  DataStructureProject
+//
+//  Created by Wortmann, Brandon on 2/8/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include <assert.h>
+#inclde "../../Nodes/LinearNode.hpp"
+
+template <class Type>
+class List
+{
+protected:
+    int size;
+    
+public:
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+    
+};
+
+#endif /* List_hpp */
