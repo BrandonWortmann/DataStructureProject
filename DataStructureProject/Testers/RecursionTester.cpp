214dbea717445aecd189d522e82a965bc96484f8
BrandonWortmann <31518730+BrandonWortmann@users.noreply.github.com>
2018-03-05 11:43:56 -0700

    I wrote test recursion

---
+++ DataStructureProject/Testers/RecursionTester.cpp
@@ -0,0 +1,32 @@
+//
+//  RecursionTester.cpp
+//  DataStructureProject
+//
+//  Created by Wortmann, Brandon on 3/5/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "RecursionTester.hpp"
+
+int RecursionTester :: fib(int number)
+{
+    
+    return 0;
+}
+
+string RecursionTester :: mystery(string word)
+{
+    
+    return "";
+}
+
+void RecursionTester :: testRecursionNumbers()
+{
+    
+}
+
+void RecursionTester :: testRecursionString()
+{
+    
+}
+
